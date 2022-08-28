dc05688a155674326bc3da853d6681200c4d0ec1
Uday Pratap Singh <uday707@gmail.com>
2022-08-28 14:11:03 +0530

    Removing log4j vulnerability & adding array example

---
+++ src/test/java/com/eric/ds/arrays/MaximumOfSubarrayOfSizeKTest.java
@@ -0,0 +1,27 @@
+package com.eric.ds.arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MaximumOfSubarrayOfSizeKTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(MaximumOfSubarrayOfSizeKTest.class);
+	private MaximumOfSubarrayOfSizeK msa;
+	
+	@Before
+	public void setUp() {
+		msa = new MaximumOfSubarrayOfSizeK();
+	}
+	
+	@Test
+	public void testForPositiveSubArrayK() {
+	   logger.info("Testing for positive values");
+	   int input[] = {8, 5, 10, 7, 9, 4, 15, 12, 90, 13};
+	   int max[] = msa.maxSubArray(input, 4);
+	   for(Integer i: max)
+	   System.out.println(i);
+	}
+}
